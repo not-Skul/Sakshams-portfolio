6ad71f6cecfbb3a1f9b1a16a9271d5030ed59856
not-Skul <142929238+not-Skul@users.noreply.github.com>
2025-06-05 12:17:04 +0530

    Update Projects.tsx

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -30,7 +30,7 @@ const Projects = () => {
       title: "MonkeyPox Detection website",
       description: "A model integrated into streamlit website for predicting monkeypox in a given photo.",
       image: "https://images.unsplash.com/photo-1706639449172-e490ead545ba?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bW9ua2V5cG94fGVufDB8fDB8fHww",
-      technologies: ["Machine Learning", "Streamlit","Kaggle],
+      technologies: ["Machine Learning", "Streamlit","Kaggle"],
       github: "#",
       demo: "#"
     }
